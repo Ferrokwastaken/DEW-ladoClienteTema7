f4c1d18e717e88406d4c0a760277b2bdcbd23585
Ferrokwastaken <israelduquesilva@hotmail.com>
2025-02-10 20:10:27 +0000

    first commit

---
+++ src/plantilla.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="parent">
+    <div class="header">
+      <h1>Datos mundiales</h1>
+    </div>
+    <div class="name"></div>
+    <div class="div-codes"></div>
+    <div class="selected"></div>
+    <div class="country-data"></div>
+    <div class="options"></div>
+    <div class="chart"></div>
+  </div>
+</template>
